a7172f5aac321dc7d88ee8a1b08cd609d9fd9508
Nelly31 <nelly_main@hotmail.com>
2019-06-25 21:45:43 +0000

    linting

--- w1/lotide/eqArrays.js
+++ w1/lotide/eqArrays.js
@@ -8,17 +8,20 @@ const assertEqual = function(actual,expected) {
   }
 };
 
-let eqArrays = function(firstArray,secondArray){
-let truthfulness = true
-for (let i = 0; i < firstArray.length; i++){
-  for (let j = 0; j < secondArray.length; j++){
-    if (secondArray[i] !== firstArray[i]){
-      truthfulness = false
-      break;
+
+let eqArrays = function(firstArray,secondArray) {
+  let truthfulness = true;
+  // looping through the first array
+  for (let i = 0; i < firstArray.length; i++) {
+  //looping second array
+    for (let j = 0; j < secondArray.length; j++) {
+      if (secondArray[i] !== firstArray[i]) {
+        truthfulness = false;
+        break;
+      }
     }
-  } 
-} return(truthfulness);
-}
+  } return truthfulness;
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
 
